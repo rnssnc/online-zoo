137b9bb97518aa7353a5e55a872b99f0f3d6e274
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-03-24 21:53:39 +0300

    feat: theme change now save data in localStorage

--- src/components/toggle/toggle.js
+++ src/components/toggle/toggle.js
@@ -5,4 +5,13 @@ const themeSwicher = document.querySelector('.theme-switch__checkbox');
 themeSwicher.addEventListener('change', function () {
   if (this.checked) document.documentElement.dataset.colorMode = 'dark';
   else document.documentElement.dataset.colorMode = 'light';
+  localStorage.setItem('theme', document.documentElement.dataset.colorMode);
+});
+
+window.addEventListener('load', () => {
+  if (localStorage.key('theme')) {
+    const theme = localStorage.getItem('theme');
+    document.documentElement.dataset.colorMode = theme;
+    if (theme === 'dark') themeSwicher.checked = true;
+  }
 });
