4e7d34ef44e12a5aff383aa915d14453bf51a567
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-04-08 14:52:14 +0300

    feat: add mobile nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,3 +3,13 @@ import '../navigation/navigation';
 import '../toggle/toggle';
 
 import './header.sass';
+
+/* mobile header */
+
+const navButton = document.querySelector('.mobile-nav');
+const nav = document.querySelector('.navigation-header-main');
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('mobile-nav--active');
+  nav.classList.toggle('navigation-header-main--active');
+});
